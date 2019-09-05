29499bed960e92feb789bb63e94653c89f95dacb
Vitalii Latysh <vitalii.latysh@globallogic.com>
2019-09-05 23:35:20 +0300

    Part#2. step#10. Created Calculator with public add, div, mult, sub, sqrt methods
    Part#2. step#4. Added sub (subtraction) implementation method.
    
    Part#2. step#5. Added div(division) implementation.
    
    Part#2. step#6. Added mult (multiplication) implementation.
    
    Part#2. step#9. Added sqrt implementation.

---
+++ Module2/HW1/src/main/java/com/epam/cdp/hw1/Calculator.java
@@ -0,0 +1,23 @@
+package com.epam.cdp.hw1;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public double div(int a, int b) {
+        return (double) a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(int a){
+        return Math.sqrt(a);
+    }
+}
