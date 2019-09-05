faec2f8743436c7431af21784a81a089f50444ff
Vitalii Latysh <vitalii.latysh@globallogic.com>
2019-09-06 00:43:30 +0300

    Part#2. step#14. Added implementation for add operation with 4 parameters. The first line of method “This is add method for 4 params”

--- Module2/HW1/src/main/java/com/epam/cdp/hw1/Calculator.java
+++ Module2/HW1/src/main/java/com/epam/cdp/hw1/Calculator.java
@@ -11,6 +11,11 @@ public class Calculator {
         return a + b + c;
     }
 
+    public int add(int a, int b, int c, int d) {
+        System.out.println("This is add method for 4 params");
+        return a + b + c + d;
+    }
+
     public int sub(int a, int b) {
         return a - b;
     }
