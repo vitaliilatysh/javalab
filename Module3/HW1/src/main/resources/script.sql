b6204637f5c0933fbd828a9847dae79a9e0a96b1
Vitalii Latysh <vitalii.latysh@globallogic.com>
2019-10-17 01:30:21 +0300

    Added diagram and trigger updated_datetime

--- Module3/HW1/src/main/resources/script.sql
+++ Module3/HW1/src/main/resources/script.sql
@@ -4,11 +4,15 @@ create table students(
    surname              text                    not null,
    date_of_birth        date                    not null,
    primary_skill        text,
-   created_datetime     date,
-   updated_datetime     date,
-   phone_number         text
+   created_datetime     timestamp               WITH TIME ZONE DEFAULT current_timestamp,
+   updated_datetime     timestamp               WITH TIME ZONE
 
 );
+
+create table phones(
+    phone_number        text primary key        not null,
+    student_id          integer                 references students(id) on delete cascade on update cascade
+)
 
 create table subjects(
    id                   integer primary  key    not null,
@@ -22,3 +26,41 @@ create table exam_results(
    subject_id           integer                 references subjects(id) on delete cascade on update cascade,
    mark                 integer
 );
+
+insert into students values (123, 'David', 'Craig', '1988-12-01', 'Java', current_timestamp);
+
+insert into students values (124, 'Bartosh', 'Kurek', '1988-10-05', 'Volleyball', current_timestamp);
+
+update students set student_name = 'Pavel' where  students.id = 123;
+
+update students set student_name = 'Daniel' where  id = 124;
+
+delete  from students;
+
+select * from students;
+
+CREATE OR REPLACE FUNCTION log_last_name_changes()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+	IF old.student_name <> new.student_name OR
+	old.surname <> new.surname OR
+	old.date_of_birth <> new.date_of_birth OR
+	old.primary_skill <> new.primary_skill THEN
+   UPDATE students SET updated_datetime = now() where id=new.id or
+   student_name=new.student_name or
+   surname=new.surname or
+   date_of_birth=new.date_of_birth or
+   primary_skill=new.primary_skill;
+   end if;
+   RETURN NEW;
+END;
+$BODY$
+LANGUAGE PLPGSQL;
+
+CREATE TRIGGER last_name_changes
+    AFTER UPDATE
+    ON students
+	for each row
+    EXECUTE PROCEDURE log_last_name_changes();
+
