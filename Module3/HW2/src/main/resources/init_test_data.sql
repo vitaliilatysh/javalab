64823c778809138dc121aa858c7149786cd229dc
Vitalii Latysh <vitalii.latysh@globallogic.com>
2019-10-25 01:17:35 +0300

    Init module3 hw2

---
+++ Module3/HW2/src/main/resources/init_test_data.sql
@@ -0,0 +1,27 @@
+insert into students (student_name, surname,date_of_birth, primary_skill, created_datetime) values
+                            ('David', 'Craig', '1988-12-01', 'Java', current_timestamp),
+                            ('Daniel', 'Camp', '1996-11-05', 'PHP', current_timestamp),
+                            ('Mark', 'Jader', '1994-07-28', 'SQL', current_timestamp),
+                            ('Antonio', 'M.', '1988-12-01', 'Java', current_timestamp),
+                            ('Pavlo', 'M', '1988-12-01', 'Java', current_timestamp),
+                            ('Ievgen', '', '1988-12-01', 'Java', current_timestamp),
+                            ('Derek', '.', '1988-12-01', 'Java', current_timestamp),
+                            ('Camel', 'Chimp', '1999-01-05', 'C++', current_timestamp);
+
+insert into subjects (subject_name, tutor) values   ('Algorithms', 'Barber'),
+                                                    ('Programming basics', 'Drain'),
+                                                    ('Data science', 'Pedro'),
+                                                    ('Data structures', 'Martin');
+
+
+insert into exam_results (student_id,subject_id,mark) values    (1, 1, 2),
+                                                                (1, 2, 2),
+                                                                (1, 3, 8),
+                                                                (2, 1, 8),
+                                                                (2, 2, 10),
+                                                                (2, 3, 2),
+                                                                (3, 1, 2),
+                                                                (3, 2, 2),
+                                                                (3, 3, 2),
+                                                                (4, 1, 8),
+                                                                (4, 4, 8);
---
+++ Module3/HW2/src/main/resources/script.sql
@@ -0,0 +1,25 @@
+-- 1. Select all primary skills that contain more than one word
+-- (please note that both ‘-‘ and ‘ ’ could be used as a separator)
+
+select * from students where array_length(regexp_split_to_array(primary_skill, '[\s|-]'),1) > 1;
+
+-- 2. Select all students who does not have second name
+-- (it is absent or consists from only one letter/letter with dot)
+
+select * from students where char_length(surname) in (0,1) or surname ~ '\w\.';
+
+-- 3. Select number of students passed exams for each subject and order result by number of student descending.
+
+select subjects.subject_name, count(students.student_name) as "how much passed" from subjects
+	inner join exam_results on subjects.id=exam_results.subject_id
+	inner join students on students.id=exam_results.student_id
+	group by subjects.subject_name order by "how much passed" desc
+
+-- 4. Select number of students with the same exam marks for each subject
+
+select subjects.subject_name, count(distinct exam_results.mark) as "how much have same mark" from subjects
+	inner join exam_results on subjects.id=exam_results.subject_id
+	inner join students on students.id=exam_results.student_id
+	group by subjects.subject_name order by "how much have same mark" desc
+
+-- 5. Select students who passed at least two exams for different subject
