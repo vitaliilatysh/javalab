8f55668cba3561fe41f6b6e70229e79169cb1db8
Vitalii Latysh <vitalii.latysh@globallogic.com>
2019-11-13 00:46:40 +0200

    Init tests

---
+++ Module3/HW4/src/test/java/com/epam/cdp/hw4/services/base/EmployeeServiceTest.java
@@ -0,0 +1,39 @@
+package com.epam.cdp.hw4.services.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class EmployeeServiceTest {
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void addEmployeeToUnit() {
+    }
+
+    @Test
+    public void findAllQa() {
+    }
+
+    @Test
+    public void findAllDev() {
+    }
+}
